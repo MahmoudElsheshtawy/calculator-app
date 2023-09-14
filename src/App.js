f84a8d0ad9b894d32ea23f8ede55cecd80b10fc0
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-09-14 13:25:45 +0300

    finsh

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import React, { useState } from 'react'
 
 function App() {
   const [state, setstate] =useState("")
+ 
 
    const handleClick =(e)=>{
-      setstate(state.concat(e.target.value))
+      setstate(state.concat(e.target.value));
+      // setCalc(calc.concat(e.target.value));
 
    }
   //  Clear 
@@ -20,51 +22,54 @@ function App() {
   }
   // cacolater =
   const Calchandelar =()=>{
-   try {
-      setstate(eval(state).toString());
-    }catch(err) {
-      setstate("Errorrrrr")
-    }
-    
-  }
-  
+    try {
+       setstate(eval(state).toString());
+     }catch(err) {
+       setstate("Errorrrrr")
+     }
+     
+   }
+
+
   // 465.999.999.
-  const integer_formater = new Intl.NumberFormat("en-us" ,{
-    maximumFractionDigits:0,
-  })
-  function formatopration(opernd) {
-    if (opernd == null) return 
-      const [integer,decimal]= opernd.split('.')
-      if(decimal == null) return integer_formater.format(integer)
+  // const integer_formater = new Intl.NumberFormat("en-us" ,{
+  //   maximumFractionDigits:0,
+  // })
+  // function formatopration(operand) {
+  //   if (operand == null) return 
+  //     const [integer,decimal]= operand.split('.')
+  //     if(decimal == null) return integer_formater.format(integer)
  
-  }
+  // }
+ 
   
   return (
     <div className="App">
                <div  className="container">
-                <form>
-                  <input type='text'  value={formatopration(state)}/>
-                    
+                <form action=''   >
+                {/* ,formatopration(state)  */}
+                  <input type='text'  value={state} />
+                 
                   </form>
-                  <div className='key'>
+                  <div className='key' >
                  <button className='hightlight' id='clear' onClick={handleremove} >Clear</button>
                  <button className='hightlight' id="backsteep" onClick={handleback}>AC</button>
-                 <button  className='hightlight' value={'/'} onClick={handleClick}>&divide;</button>
-                 <button value={'7'} onClick={handleClick}>7</button>
-                 <button value={'8'} onClick={handleClick}>8</button>
+                 <button  className='hightlight' value={"/"}  onClick={handleClick}>&divide;</button>
+                 <button  value={"7"} onClick={handleClick}>7</button>
+                 <button value={"8"} onClick={handleClick}>8</button>
                  <button value={'9'} onClick={handleClick}>9</button>
-                 <button  className='hightlight' value={'*'} onClick={handleClick}>&times;</button>
-                 <button value={'4'} onClick={handleClick}>4</button>
+                 <button  className='hightlight' name={"*"} onClick={handleClick}>*</button>
+                 <button value={"4"} onClick={handleClick}>4</button>
                  <button value={'5'} onClick={handleClick}>5</button>
                  <button value={'6'} onClick={handleClick}>6</button>
-                 <button  className='hightlight' value={'-'} onClick={handleClick}>&ndash;</button>
+                 <button  className='hightlight' name={"-"} onClick={handleClick}>&ndash;</button>
                  <button value={'1'} onClick={handleClick}>1</button>
                  <button value={'2'} onClick={handleClick}>2</button>
                  <button value={'3'} onClick={handleClick}>3</button>
-                 <button  className='hightlight' value={'+'} onClick={handleClick}>+</button>
+                 <button  className='hightlight' name={"+"} onClick={handleClick}>+</button>
                  <button value={'0'} onClick={handleClick}>0</button>
                  <button value={'.'} onClick={handleClick}>.</button>
-                 <button  onClick={Calchandelar} className="tow hightlight">=</button>
+                 <button onClick={Calchandelar}  className="tow hightlight">=</button>
 
                 </div>
 
