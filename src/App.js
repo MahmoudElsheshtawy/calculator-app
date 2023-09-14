fdcf0aa0d6dc445a9d9fc78926f0e585a0f29342
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-09-14 13:45:08 +0300

    d

--- src/App.js
+++ src/App.js
@@ -58,15 +58,15 @@ function App() {
                  <button  value={"7"} onClick={handleClick}>7</button>
                  <button value={"8"} onClick={handleClick}>8</button>
                  <button value={'9'} onClick={handleClick}>9</button>
-                 <button  className='hightlight' name={"*"} onClick={handleClick}>*</button>
+                 <button  className='hightlight' value={"*"} onClick={handleClick}>*</button>
                  <button value={"4"} onClick={handleClick}>4</button>
                  <button value={'5'} onClick={handleClick}>5</button>
                  <button value={'6'} onClick={handleClick}>6</button>
-                 <button  className='hightlight' name={"-"} onClick={handleClick}>&ndash;</button>
+                 <button  className='hightlight' value={"-"} onClick={handleClick}>&ndash;</button>
                  <button value={'1'} onClick={handleClick}>1</button>
                  <button value={'2'} onClick={handleClick}>2</button>
                  <button value={'3'} onClick={handleClick}>3</button>
-                 <button  className='hightlight' name={"+"} onClick={handleClick}>+</button>
+                 <button  className='hightlight' value={"+"} onClick={handleClick}>+</button>
                  <button value={'0'} onClick={handleClick}>0</button>
                  <button value={'.'} onClick={handleClick}>.</button>
                  <button onClick={Calchandelar}  className="tow hightlight">=</button>
